34d2f4bd7831c3f784b117e9204592dd91dc94d9
V-Nar <valery.narboni@gmail.com>
2022-08-18 18:02:29 +0200

    completed lab

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+// my own recipe
+const myRecipe = {
+  title: 'Lime mascarpone mousse',
+  level: 'Easy Peasy',
+  ingredients: ['lime', 'sugar', 'mascarpone', 'liquid cream'],
+  dishType: 'dessert',
+  duration: 20,
+  creator: 'Valéry',
+}
+
 // Connection to the database "recipe-app"
+
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
@@ -15,9 +26,37 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then( async () => {
+    // iteration 2
+    const newRecipe = await Recipe.create(myRecipe)
+    console.log('title: ', newRecipe.title)
+
+    // iteration 3
+    const recipeDB = await Recipe.insertMany(data)
+    recipeDB.forEach(recipe => {
+      console.log(recipe.title)
+    }) 
+    // iteration 4
+    const updateDuration = await Recipe.findOneAndUpdate(
+      {
+        title: 'Rigatoni alla Genovese',
+      },
+      { duration: 100 },
+      { new: true },
+    )
+    console.log(`Rigatoni alla Genovese duration updated to ${updateDuration.duration}`)
+
+    // iteration 5
+    const deletedRecipe = await Recipe.deleteOne(
+      {
+        title: 'Carrot Cake',
+      },
+    )
+    // iteration 6
+  mongoose.connection.close()
   })
+    
+
   .catch(error => {
-    console.error('Error connecting to the database', error);
+    console.error('Error connecting to the database', error.message);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: Schema.Types.String,
+  level: {
+    type: Schema.Types.String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+  },
+  ingredients: [Schema.Types.String],
+  cuisine: Schema.Types.String,
+  dishType: {
+    type: Schema.Types.String,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other'],
+  },
+  image: {
+    type: Schema.Types.String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: {
+    type: Schema.Types.Number,
+    min: 0,
+  },
+  creator: Schema.Types.String,
+  created: {
+    type: Schema.Types.Date,
+    default: Date,
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
